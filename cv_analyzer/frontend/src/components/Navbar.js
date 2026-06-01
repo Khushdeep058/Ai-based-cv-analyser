4a8c1472ca708df2cb1a32aef9bd2c71d4c426fc
Khushdeep058 <160999398+Khushdeep058@users.noreply.github.com>
2026-06-01 23:31:34 +0530

    Update Navbar.js

--- cv_analyzer/frontend/src/components/Navbar.js
+++ cv_analyzer/frontend/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Menu, X, ArrowRight, Moon, Sun } from 'lucide-react';
 
 const Navbar = () => {
